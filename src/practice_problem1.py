f975d5c4d9dfb77eef07c4189d89b6f837294a98
fusarocr <fusarocr@rose-hulman.edu>
2018-01-09 20:01:02 -0500

    remember return a new object use object name

--- src/practice_problem1.py
+++ src/practice_problem1.py
@@ -47,7 +47,7 @@ def main():
     run_test_reset()
     run_test_steal()
     run_test_get_history()
-#     run_test_combined_box()
+    run_test_combined_box()
 
 
 ########################################################################
@@ -480,7 +480,7 @@ class Box(object):
           :type other_box: Box
         """
         # --------------------------------------------------------------
-        # TODO: 10. Implement and test this function.
+        # DONE: 10. Implement and test this function.
         #     The testing code is already written for you (above).
         # --------------------------------------------------------------
         # --------------------------------------------------------------
@@ -488,7 +488,8 @@ class Box(object):
         #    DIFFICULTY:      4
         #    TIME ESTIMATE:   5 minutes.
         # --------------------------------------------------------------
-
+        return Box(self.contents + other_box.contents, self.volume +
+                    other_box.volume)
 
 ########################################################################
 # The TEST functions for the  Box  class begin here.
